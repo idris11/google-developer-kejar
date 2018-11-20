f15e730b3dc6197c8094b7cb6d1a00d98f37ad0c
Idris <idrisnovandi@gmail.com>
2018-11-20 14:48:19 +0700

    1st commit

---
+++ public/operation.js
@@ -0,0 +1,11 @@
+var firstNumber = document.getElementById("first").value 
+var secondNumber = document.getElementById("second").value 
+
+
+    
+function process(){
+    console.log(firstNumber,secondNumber)
+}
+function operation(){
+    
+}
